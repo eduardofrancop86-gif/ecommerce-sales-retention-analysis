cbcffcb91c4221d4d7a9ef785533254e6c5ba044
eduardofrancop86-gif <eduardo.francop86@gmail.com>
2026-06-15 11:57:32 -0600

    Create query_analisis_mensual.sql

---
+++ query_analisis_mensual.sql
@@ -0,0 +1,23 @@
+SELECT
+EXTRACT(YEAR FROM created_at) AS anio,
+EXTRACT(MONTH FROM created_at) AS mes,
+
+SUM(sale_price) AS ingresos_totales,
+
+COUNT(DISTiNCT order_id) AS total_ordenes,
+COUNT(DISTINCT user_id) AS total_clientes,
+ROUND(COUNT(DISTINCT order_id) / COUNT(DISTINCT user_id), 2) AS ordenes_por_cliente
+
+FROM
+`bigquery-public-data.thelook_ecommerce.order_items`
+
+GROUP BY
+anio,
+mes
+
+HAVING
+ ingresos_totales > 10000
+
+ORDER BY
+anio DESC,
+mes DESC;
